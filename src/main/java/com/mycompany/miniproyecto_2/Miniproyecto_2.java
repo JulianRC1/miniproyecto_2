fe694fbd6941e54fa71ff48ae90ae0cfae25518e
Julian Rendon <juldarecar2003@gmail.com>
2022-11-06 20:58:23 -0500

    Inicio del miniproyecto

--- src/main/java/com/mycompany/miniproyecto_2/Miniproyecto_2.java
+++ src/main/java/com/mycompany/miniproyecto_2/Miniproyecto_2.java
@@ -5,13 +5,25 @@
 
 package com.mycompany.miniproyecto_2;
 
+import javax.swing.JFrame;
+
+
 /**
- *
- * @author julda
- */
+/*
+* FUNDAMENTOS DE PROGRAMACIÓN ORIENTADO A EVENTOS
+* MINIPROYECTO 2
+* PROFESOR: Luis Romo Portilla
+* 
+* INTEGRANTES: Julian David Rendon Cardona <julian.david.rendon@gmail.com> 202177387
+*              Carol Sofia Macarena Rubiano Valderrama
+*              Juan Sebastian Muñoz Rojas
+*/
+
 public class Miniproyecto_2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        VentanaPrincipal ventana = new VentanaPrincipal();
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
     }
 }
